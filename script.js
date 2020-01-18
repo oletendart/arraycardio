1816446f2452709bccae5730291f69100f620ba4
Olivia Letendart <letendartolivia@gmail.com>
2020-01-18 14:27:21 +0100

    rectification const 'de' new method filter

--- script.js
+++ script.js
@@ -105,4 +105,6 @@ console.table(oldest);
 const category = document.querySelector(".mw-category");
 const links = Array.from(category.querySelectorAll("a"));
 
-const de = links.map(link => link.textContent);
+const de = links
+  .map(link => link.textContent)
+  .filter(streetName => streetName.includes("de"));
